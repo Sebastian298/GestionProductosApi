79faf908eedc2546e8146a42b6428c73d893dfec
sebastian <jaimetenorio_geografia@outlook.es>
2021-12-12 17:36:06 -0600

    Se termino el modulo de marcas realizando la conexion a los endpoints correspondientes de la api rest

--- GestionProductosApi/Controllers/BrandsController.cs
+++ GestionProductosApi/Controllers/BrandsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GestionProductosApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -8,6 +9,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GestionProductosApi.Controllers
@@ -19,6 +21,8 @@ namespace GestionProductosApi.Controllers
         public BrandsController(IConfiguration configuration)
         {
             Configuration = configuration;
+            http.DefaultRequestHeaders.Clear();
+            http.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", Configuration["Authorization:Token"]);
         }
         public IActionResult Index()
         {
@@ -30,12 +34,40 @@ namespace GestionProductosApi.Controllers
         {
             var CompanyID = HttpContext.Session.GetString("CompanyID");
             var url = $"https://localhost:44326/api/brands/GetBrands/{CompanyID}";
-            http.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", Configuration["Authorization:Token"]);
             var result = http.GetAsync(url).Result;
             var resultContent = result.Content.ReadAsStringAsync().Result;
             dynamic res = JsonConvert.DeserializeObject(resultContent);
             res = JObject.Parse(res.ToString());
             return Json(res);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBrand([FromBody] Brand objBrand)
+        {
+            var CompanyID = HttpContext.Session.GetString("CompanyID");
+            var url = "https://localhost:44326/api/brands/Create";
+            dynamic jsonRequest = new JObject();
+            jsonRequest.Name = objBrand.Name;
+            jsonRequest.Image = objBrand.Image;
+            jsonRequest.CompanyID = CompanyID;
+            var content = new StringContent(jsonRequest.ToString(), Encoding.UTF8, "application/json");
+            var result = http.PostAsync(url, content).Result;
+            dynamic resultContent = result.Content.ReadAsStringAsync().Result.ToString();
+            return Json(resultContent.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult UpdateBrand([FromBody] Brand objBrand)
+        {
+            var url = "https://localhost:44326/api/brands/Update";
+            dynamic jsonRequest = new JObject();
+            jsonRequest.BrandID = objBrand.BrandID;
+            jsonRequest.Name = objBrand.Name;
+            jsonRequest.Image = objBrand.Image;
+            var content = new StringContent(jsonRequest.ToString(), Encoding.UTF8, "application/json");
+            var result = http.PutAsync(url, content).Result;
+            dynamic resultContent = result.Content.ReadAsStringAsync().Result.ToString();
+            return Json(resultContent.ToString());
         }
     }
 }
--- GestionProductosApi/wwwroot/js/brand.js
+++ GestionProductosApi/wwwroot/js/brand.js
@@ -14,21 +14,154 @@ function getBrands() {
         .then(data => {
             $('#example').DataTable().clear().destroy();
             bodyBrands.innerHTML = ``;
-            data.Content.forEach((res) => {
-                html += `
+            if (data.Title == 'Unauthorized') {
+                Swal.fire({
+                    icon: 'error',
+                    text: `${data.Description}`
+                }).then(() => {
+                    bodyBrands.innerHTML += html;
+                    $('#example').DataTable();
+                    return;
+                })
+            } else {
+                data.Content.forEach((res) => {
+                    html += `
                    <tr>
                    <td>${res.Name}</td>
                    <td><img src="${res.Image}" class="img-responsive" width="50" height="50"/></td>
                    <td><a><i class="far fa-edit" data-toggle="modal" data-target="#modalEdit" onclick="writeData('${res.BrandID}','${res.Name}','${res.Image}')"></i></a></td>
                 `
-            })
-            bodyBrands.innerHTML += html;
-            $('#example').DataTable();
+                })
+                bodyBrands.innerHTML += html;
+                $('#example').DataTable();
+            }
+            
         })
+}
+
+function addBrand() {
+    if (brandName.value != '') {
+        let objBrand = {
+            Name: brandName.value,
+            Image: brandImage.value
+        };
+        objBrand = JSON.stringify(objBrand);
+        fetch("Brands/CreateBrand", {
+            method: "POST",
+            body: objBrand,
+            headers: {
+                'Accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        }).then(response => response.json())
+            .then(data => {
+                data = JSON.parse(data);
+                console.log(data)
+                if (data.Success == true) {
+                    Swal.fire({
+                        icon: 'success',
+                        text: `${data.Description}`
+                    }).then(() => {
+                        $('#modalAdd').modal('hide');
+                        getBrands();
+                        brandName.value = '';
+                        brandImage.value = '';
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        text: `${data.Description}`
+                    }).then(() => {
+                        $('#modalAdd').modal('hide');
+                        brandName.value = '';
+                        brandImage.value = '';
+                    })
+                }
+            });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            text: `Debe definir un nombre de marca`
+        });
+    }
+}
+
+function editBrand() {
+    if (brandNameEdit.value != '') {
+        let objBrand = {
+            BrandID: brandID.value,
+            Name: brandNameEdit.value,
+            Image: brandImageEdit.value
+        };
+        objBrand = JSON.stringify(objBrand);
+        fetch("Brands/UpdateBrand", {
+            method: "POST",
+            body: objBrand,
+            headers: {
+                'Accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        }).then(response => response.json())
+            .then(data => {
+                data = JSON.parse(data);
+                console.log(data)
+                if (data.Success == true) {
+                    Swal.fire({
+                        icon: 'success',
+                        text: `${data.Description}`
+                    }).then(() => {
+                        $('#modalEdit').modal('hide');
+                        getBrands();
+                        brandNameEdit.value = '';
+                        brandImageEdit.value = '';
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        text: `${data.Description}`
+                    }).then(() => {
+                        $('#modalEdit').modal('hide');
+                        brandNameEdit.value = '';
+                        brandImageEdit.value = '';
+                    })
+                }
+            });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            text: `Debe definir un nombre de marca`
+        });
+    }
 }
 
 function writeData(BrandID, Name, Image) {
     brandID.value = BrandID;
     brandNameEdit.value = Name;
     brandImageEdit.value = Image;
+}
+
+function validar() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: '¿Estas seguro(a)?',
+        text: "Los datos de la marca seran editados",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, ¡Editar!',
+        cancelButtonText: 'No, 	¡Cancelar!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.value) {
+            editBrand();
+        } else {
+            $('#modalEdit').modal('hide');
+        }
+    })
 }
